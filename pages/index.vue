e1801a42889a1c8f3d9804a66b5728b1958cf4f3
naulawrdhh <141692064+naulawrdhh@users.noreply.github.com>
2023-10-17 09:33:12 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>TGL & WKTU</th>
+                    <th>nama</th>
+                    <th>anggota</th>
+                    <th>keperluan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for=“pengunjung in visitors” :key=“pengunjung.id">
+            td>{{ pengunjung.id }}</td>
+            <td>{{ pengunjung.tgl }}</td>
+            <td>{{ pengunjunng.nama }}</td>
+            <td>{{ pengunjung.anggota }}</td>
+            <td>{{ pengunjung.keperluan}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+const client = useSupabaseClient()
+const visitors = ref([])
+
+async function getData() {
+const { data, error } = await client
+    .from(“pengunjung”)
+    .select()
+if(data) visitors.value = data
+}
+onMounted(() => {
+getData()
+})
+</script>
---
+++ pages/tambah.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>
+            <h1>Isi Buku Tamu</h1>
+                form @submit.prevent=“simpan()”>
+                <input v-model=“nama” placeholder=“nama”> <br/>
+                <input v-model=“tgl” placeholder=“instansi”> <br/>
+                <textarea v-model=“alamat” placeholder=“alamat”></textarea> <br/>
+                <button type=“submit”>Kirim</button>
+                <NuxtLink to=“/“>Kembali</NuxtLink>
+            </form>
+        </h1>
+    </div>
+</template>
