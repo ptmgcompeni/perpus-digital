bc232283e9bad30f96840d61fbe43bd6196293e4
naulawrdhh <141692064+naulawrdhh@users.noreply.github.com>
2023-11-01 11:42:03 +0700

    Update tambah.vue

--- pages/tambah.vue
+++ pages/tambah.vue
@@ -2,13 +2,19 @@
     <div>
         <h1>
             <h1>Isi Buku Tamu</h1>
-                form @submit.prevent=“simpan()”>
-                <input v-model=“nama” placeholder=“nama”> <br/>
-                <input v-model=“tgl” placeholder=“instansi”> <br/>
-                <textarea v-model=“alamat” placeholder=“alamat”></textarea> <br/>
-                <button type=“submit”>Kirim</button>
+                <form @submit.prevent="simpan()">
+                <input v-model="nama" placeholder=“nama”> <br/>
+                <input v-model="tgl" placeholder=“instansi”> <br/>
+                <textarea v-model="alamat" placeholder=“alamat”></textarea> <br/>
+                <button type="submit">Kirim</button>
                 <NuxtLink to=“/“>Kembali</NuxtLink>
             </form>
         </h1>
     </div>
 </template>
+
+<script setup>
+const nama = ref("")
+const tgl = ref("")
+const alamat = ref("")
+</script>
