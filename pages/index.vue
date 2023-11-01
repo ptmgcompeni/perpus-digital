25be0aaa059ec3819d4d610d73db6ccf8c6a617c
naulawrdhh <141692064+naulawrdhh@users.noreply.github.com>
2023-11-01 11:40:40 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,35 @@
 <template>
     <div>
-        <table>
+        <table border="1" width="100%">
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>TGL & WKTU</th>
+                    <th>TGL</th>
                     <th>nama</th>
                     <th>anggota</th>
                     <th>keperluan</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for=“pengunjung in visitors” :key=“pengunjung.id">
-            td>{{ pengunjung.id }}</td>
-            <td>{{ pengunjung.tgl }}</td>
-            <td>{{ pengunjunng.nama }}</td>
-            <td>{{ pengunjung.anggota }}</td>
-            <td>{{ pengunjung.keperluan}}</td>
-            </tr>
+                <tr v-for="pengunjung in visitors" :key="pengunjung.id">
+                    <td>{{ pengunjung.id }}</td>
+                    <td>{{ pengunjung.tgl }}</td>
+                    <td>{{ pengunjung.nama }}</td>
+                    <td>{{ pengunjung.anggota }}</td>
+                    <td>{{ pengunjung.keperluan }}</td>
+                </tr>
             </tbody>
         </table>
     </div>
 </template>
 
 <script setup>
-const client = useSupabaseClient()
+const supabase = useSupabaseClient()
 const visitors = ref([])
 
 async function getData() {
-const { data, error } = await client
-    .from(“pengunjung”)
+const { data, error } = await supabase
+    .from('pengunjung')
     .select()
 if(data) visitors.value = data
 }
