44c5073aaa9c9013330cc152ecdfcf3959aba87f
naulawrdhh <141692064+naulawrdhh@users.noreply.github.com>
2023-11-14 08:59:17 +0700

    Add files via upload

--- pages/tambah.vue
+++ pages/tambah.vue
@@ -1,20 +1,35 @@
 <template>
     <div>
-        <h1>
-            <h1>Isi Buku Tamu</h1>
-                <form @submit.prevent="simpan()">
-                <input v-model="nama" placeholder=“nama”> <br/>
-                <input v-model="tgl" placeholder=“instansi”> <br/>
-                <textarea v-model="alamat" placeholder=“alamat”></textarea> <br/>
-                <button type="submit">Kirim</button>
-                <NuxtLink to=“/“>Kembali</NuxtLink>
-            </form>
-        </h1>
+        <h1>Isi pengunjung</h1>
+        <form>
+            <input v-model="form.nama" type="text"> placeholder= "Nama..." required><br/>
+            <select v-model="form.anggota">
+                <option value="">pilih keanggotaan</option>
+                <option v-for = "a in anggota" :key="a.id" .value="a.id">{{ a.nama }}</option>
+            </select> <br>
+            <textarea v-model="form.keperluan" cols="30" rows="10" placeholder="keperluan..."></textarea> <br/>
+            <button type="submit">Kirim</button> -
+            <NuxtLink to="/">Kembali</NuxtLink>
+        </form>
     </div>
 </template>
 
+
 <script setup>
-const nama = ref("")
-const tgl = ref("")
-const alamat = ref("")
+const supabase = useSupabaseClient()
+const form = reactive({
+    nama: "",
+    anggota: "",
+    keperluan: "",
+})
+
+async function getAnggota() {
+    const { data, error } = await supabase.from('anggota').select()
+    if(data) anggota.value=data
+}
+async function SimpanKunjungan() {
+    const { error } = await supabase.from('pengunjung').insert(from)
+}
+
+onMounted(() => getAnggota())
 </script>
